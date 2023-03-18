b2841caa607152e38993ce7687ee2a1c3f799a8a
Pedro Bento <pedrobento.informatica@gmail.com>
2023-03-18 19:23:03 -0300

    feat: basic web scraper

--- 2-web-scraping-projects/2.1-beautiful-soap/2.1.1-basic-web-scraper/README.md
+++ 2-web-scraping-projects/2.1-beautiful-soap/2.1.1-basic-web-scraper/README.md
@@ -4,7 +4,7 @@ This is a basic web scraper project that uses Python and the Beautiful Soup libr
 
 ## Project Description
 
-The goal of this project is to introduce beginners to web scraping with Python and Beautiful Soup. The project includes a Python script that scrapes data from a website, extracts specific information from the HTML, and saves the data to a CSV file.
+The goal of this project is to introduce beginners to web scraping with Python and Beautiful Soup. The project includes a Python script that scrapes data from a website, extracts specific information(title and links) from the HTML, and saves the data to a CSV file.
 
 ## Prerequisites
 
@@ -12,12 +12,7 @@ To run this project, you will need:
 
 - Python 3.x
 - Beautiful Soup 4
-
-You can install Beautiful Soup using pip: 
-
-```
-pip install beautifulsoup4
-```
+- html5lib (Parser)
 
 ## Getting Started
 
---
+++ 2-web-scraping-projects/2.1-beautiful-soap/2.1.1-basic-web-scraper/scraper.py
@@ -0,0 +1,90 @@
+from urllib import urlopen
+from bs4 import BeautifulSoup
+from urllib.error import HTTPError, URLError
+from typing import Union
+
+
+def request_data(url: str) -> Union[BeautifulSoup, None]:
+    """Request data from an url
+
+    Args:
+        url (str): website url
+
+    Returns:
+        A BeautifulSoup object or None if an error occurred
+
+    """
+    try:
+        html = urlopen(url)
+        soup = BeautifulSoup(html.read(), "html5lib")
+
+        return soup
+    except HTTPError:
+        print("[ERROR]: Page not found!")
+    except URLError:
+        print("[ERROR]: Server not found!")
+
+
+def extract_data(soup: BeautifulSoup) -> Union[dict, None]:
+    """Extract data from a BeautifulSoup object
+
+    Args:
+        soup (BeautifulSoup): Requested data
+
+    Returns:
+        Union[dict, None]: Dict with extracted data or None if an error occurred
+    """
+    try:
+        title = soup.title.string
+        paragraphs = [link.string for link in soup.find_all("p")]
+
+        return {"Title": title, "Paragraphs": paragraphs}
+    except AttributeError:
+        print("[Error]: Tag not found!")
+
+
+def save_data(data: dict, file_name: str = "output.csv") -> bool:
+    """Save data into a file
+
+    Args:
+        data (dict): Extracted data
+        file_name (str): File name
+
+    Returns:
+        bool: Successfuly saved or not
+    """
+
+    try:
+        with open(file_name, "w", newline="") as csvfile:
+            # Write columns
+            csvfile.write(f"{','.join(data.keys())}\n")
+            # Write content
+            csvfile.write(f"{','.join(data.values())}\n")
+            # Close file
+            csvfile.close()
+
+            return True
+    except Exception as e:
+        print(f"[ERROR]: Unexpected error: {e}")
+
+    return False
+
+
+def main() -> None:
+
+    # Request data
+    url = "https://raw.githubusercontent.com/PedroFerreiraBento/Python-Projects/main/2-web-scraping-projects/2.1-beautiful-soap/static/example-1.html"
+    soup = request_data(url)
+
+    # Extract data
+    data = extract_data(soup) if soup is not None else None
+
+    # Save data
+    is_saved = save_data(data) if data is not None else None
+    
+    if is_saved:
+        print("[INFO]: Successfuly extracted data!")
+
+
+if __name__ == "__main__":
+    main()
